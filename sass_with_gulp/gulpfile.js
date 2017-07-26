abdad0056edeb6cc55b1506a90157254118ad224
chiabi88 <chiabi88@gmail.com>
2017-07-26 14:48:14 +0900

    gulp : html, js, images minify 추가

--- sass_with_gulp/gulpfile.js
+++ sass_with_gulp/gulpfile.js
@@ -1,121 +1,207 @@
-// Modules 호출
-var gulp = require('gulp'),
-
 /**
- * gulp 패키지 모듈 호출
+ * -------------------------------
+ * 모듈 호출
+ * -------------------------------
  */
-    // sass, css 관련
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename  = require('gulp-rename'),
+    htmlmin = require('gulp-htmlmin'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     sassdoc = require('sassdoc'),
-    
-    // 스프라이트 생성 모듈
+    imagemin  = require('gulp-imagemin'),
     spritesmith  = require('gulp.spritesmith'),
-
-    // 유틸리티
     sourcemaps = require('gulp-sourcemaps'),
     merge = require('merge-stream'),
-
-    // Browser 서버/싱크 
-	  browserSync  = require('browser-sync'),
-	  reload = browserSync.reload;
+    del = require('del'),
+    browserSync  = require('browser-sync'),
+    reload = browserSync.reload;
 
 /**
- * ========================
- * path : 경로 담을 객체
- * =======================
+ * -------------------------------
+ * 경로 담은 객체
+ * -------------------------------
  */
-var src = 'project/src';
-var dist = 'project/dist';
-var path = {
-  js: src + 'js/**/*.js',
-  scss: src + 'stylesheets/**/*.scss',
-  images: src + 'images/**/*'
+const DIR = {
+  SRC   : 'project/src',
+  DIST  : 'project/dist',
+  DOCS  : 'project/docs'
 };
+const SRC = {
+  JS    : DIR.SRC + '/js/**/*.js',
+  CSS   : DIR.SRC + '/stylesheets/**/*.scss',
+  HTML  : DIR.SRC + '/html/**/*.html',
+  IMAGES: DIR.SRC + '/images/*'
+};
+const DIST = {
+  JS    : DIR.DIST + '/js',
+  CSS   : DIR.DIST + '/css', 
+  HTML  : DIR.DIST + '/html',
+  IMAGES: DIR.DIST + '/images'
+}
 
 /**
- * ========================
- * config
- * =======================
+ * -------------------------------
+ * config : 각 플러그인 옵션 설정
+ * -------------------------------
  */
 var config = {
-  // 옵션 : https://github.com/sass/node-sass#options
-  'sass': {
-    errLogToConsole: true,
-    outputStyle: 'expanded'
+  // Reference : https://github.com/kangax/html-minifier#options-quick-reference
+  'html': {
+    /**
+     * outputStyle (Type : Boolean  , Default : false)
+     * CSS의 컴파일 결과 코드스타일 지정
+     */
+    collapseWhitespace: 'true'
+    /**
+     * outputStyle (Type : Boolean  , Default : false)
+     * 스타일 요소 및 스타일 속성에서 css 축소(clean-css 사용)
+     */
+    //minifyCSS: 'true',
+    /**
+     * outputStyle (Type : Boolean  , Default : false)
+     * 스크립트 요소 및 이벤트 속성에서 javascript 최소화(uglify js 사용)
+     */
+    //minifyJS: 'true'
   },
-  // 옵션 : https://github.com/postcss/autoprefixer#options
+  // Reference : https://github.com/sass/node-sass#options
+  'sass': {
+    /**
+     * outputStyle (Type : String  , Default : nested)
+     * CSS의 컴파일 결과 코드스타일 지정
+     * Values : nested, expanded, compact, compressed
+     */
+    outputStyle: 'expanded'
+    /**
+     * indentType (>= v3.0.0 , Type : String , Default : space)
+     * 컴파일 된 CSS의 "들여쓰기" 의 타입
+     * Values : space , tab
+     */
+    // indentType: 'space',
+    /**
+     * indentWidth (>= v3.0.0, Type : Integer , Default : 2)
+     * 컴파일 된 CSS의 "들여쓰기" 의 갯수
+     */
+    // indentWidth: 2,
+    /**
+     * precision (Type :  Integer , Default : 5)
+     * 컴파일 된 CSS의 소수점 자리수.
+     */
+    // precision: 5,
+    /**
+     * sourceComments (Type : Boolean , Default : false)
+     * 컴파일 된 CSS에 원본소스의 위치와 줄수 주석표시.
+     */
+    // sourceComments: false
+  },
+  // Reference : https://github.com/postcss/autoprefixer#options
   // 브라우저 리스트 참고 : https://github.com/ai/browserslist#queries
   'autoprefixer': {
     // 지원 브라우저 체크
     browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
   },
   'sassdoc': {
-    dest: 'project/sassdoc/'
+    dest: DIR.DOCS + '/sassdoc'
   },
   // Browser-sync
-  // 옵션: http://www.browsersync.io/docs/options/
+  // Reference: http://www.browsersync.io/docs/options/
 	'browserSync': { 
-		'server'  : ['dist'],
-		'port'    : 8080,
-		'notify'  : false,
+		server  : DIR.DIST,
+    port    : 8080,
+    // Don't show any notifications in the browser.
+		notify  : false,
 	}
 }
 
 /**
- * ========================
- * @task : sass compile & autoprefixer & sourcemaps
- * =======================
+ * -------------------------------
+ * task : HTML | html minify
+ * -------------------------------
+ */
+gulp.task('html', function() {
+  return gulp.src(SRC.HTML)
+             .pipe(htmlmin(config.html))
+             .pipe(gulp.dest(DIST.HTML))
+             .pipe(reload({stream: true}));
+});
+
+/**
+ * -------------------------------
+ * task : JS
+ * -------------------------------
+ */
+gulp.task('js', function() {
+  return gulp.src(SRC.JS)
+             .pipe(uglify())
+             .pipe(rename({suffix: '.min'}))
+             .pipe(gulp.dest(DIST.JS))
+             .pipe(reload({stream: true}));
+});
+
+/**
+ * -------------------------------
+ * task : CSS | sass 컴파일, sassdoc, sourcemap
+ * -------------------------------
  */
 gulp.task('sass', function() {
-  return gulp.src(path.scss)
+  return gulp.src(SRC.CSS)
              .pipe(sourcemaps.init())
              .pipe(sass(config.sass).on('error', sass.logError))
              .pipe(autoprefixer(config.autoprefixer))
              .pipe(sourcemaps.write('./maps'))
-             .pipe(gulp.dest(dist + '/css'))
+             .pipe(gulp.dest(DIST.CSS))
              .pipe(reload({stream: true}));
-});
 
+});
 gulp.task('sassdoc', function() {
-  return gulp.src(path.scss)
-             .pipe(sassdoc(sassdocOptions))
-             .resume();
+  return gulp.src(SRC.CSS)
+             .pipe(sassdoc(config.sassdoc));
+});
+/**
+ * -------------------------------
+ * task : IMAGE
+ * -------------------------------
+ */
+// task : IMG 파일 minify해서 dist 폴더에 넣기 
+gulp.task('images',['sprite'], function() {
+  return gulp.src([SRC.IMAGES, '!' + DIR.SRC + '/images/sprites'])
+             .pipe(imagemin())
+			       .pipe(gulp.dest(DIST.IMAGES));
 });
 
-// 업무: images 디렉토리 dist 디렉토리 안으로 이동
-gulp.task('images', function() {
-	return gulp
-			.src(path.images)
-			.pipe( gulp.dest(dist + '/images') );
-});
-
-// 업무: 스프라이트 이미지 생성
+// task : 스프라이트 이미지 생성
 gulp.task('sprite', function() {
-	var spriteData = gulp
-		.src(src + '/images/test/*.png')
-		.pipe( spritesmith({
-			'imgName' : 'sprite.png',
-			'cssName' : '_sprite.scss'
-		}) );
+	var spriteData = gulp.src(DIR.SRC + '/images/sprites/*.png')
+                       .pipe( spritesmith({
+                         'imgName' : 'sprite.png',
+                         'cssName' : '_sprite.scss'
+                       }) );
 
 	// return spriteData.pipe( gulp.dest('dist') );
-
-	var imgStream = spriteData.img.pipe(gulp.dest(dist + '/images'));
-	var cssStream = spriteData.css.pipe(gulp.dest(src + '/stylesheets'));
+	var imgStream = spriteData.img.pipe(gulp.dest(DIST.IMAGES));
+	var cssStream = spriteData.css.pipe(gulp.dest(DIR.SRC + '/stylesheets'));
 
 	return merge(imgStream, cssStream);
 });
 
-// watch : 관찰할 업무 지정
-gulp.task('watch', function() {
-  gulp.watch(path.scss, ['sass']);
+// task : clean | 이전 dist 폴더의 파일 삭제
+gulp.task('clean', function() {
+  return del.sync([DIR.DIST]);
 });
 
+// task : watch | 관찰할 업무 지정
+gulp.task('watch', function() {
+  gulp.watch(SRC.JS, ['js']);
+  gulp.watch(SRC.CSS, ['sass']);
+  gulp.watch(SRC.HTML, ['html']);
+  gulp.watch(SRC.IMAGES, ['images']);
+});
 
-gulp.task('browserSync', ['sass'], function () { 
+// task : browserSync
+gulp.task('browserSync', ['html', 'js', 'sass'], function () { 
   return browserSync.init(config.browserSync);
 });
 
-// 'gulp'를 실행하면 sass, watch task를 실행함
+// task : default | 'gulp'를 실행하면 sass, watch task를 실행함
 gulp.task('default', ['browserSync','watch']);
---
+++ sass_with_gulp/project/src/js/index.js
@@ -0,0 +1,10 @@
+(function($){
+  var a = 1;
+  var b = 'string';
+  var c = {
+    name: 'chiabi88',
+    gender: 'female'
+  };
+  console.log(b);
+  console.dir(c);
+}(jQuery));
